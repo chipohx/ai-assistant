df0b53c66361214a60ff58444c9df42e99d92f0e
Casey <sharus.programmer@gmail.com>
2025-06-17 02:35:18 +0400

    add: reserve_handler.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import state_handlers.view_event_data
 import state_handlers.edit_data
 import state_handlers.setup_end
 import state_handlers.idle
+import state_handlers.reserve_handler # Он обязательно должен быть в конце, чтобы не мешать другим обработчикам
 
 # Запуск работы бота - - - - - - - - - - - - - - - - - - - - - -  - - -
 
