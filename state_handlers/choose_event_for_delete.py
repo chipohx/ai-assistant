53724668863ade50ad57db118e621e99c6d442a3
Casey <sharus.programmer@gmail.com>
2025-06-17 03:21:58 +0400

    add: choose_event_for_delete.py

---
+++ state_handlers/choose_event_for_delete.py
@@ -0,0 +1,15 @@
+from loguru import logger
+logger.info("Adding greetings handler...")
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+
+from telebot import types
+import storage
+
+bot = storage.get_value('bot')
+state_machine = storage.get_value('state_machine')
+csrftoken = storage.get_value('csrftoken')
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
